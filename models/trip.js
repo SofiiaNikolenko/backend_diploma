f15d99ab0ad711a3f8d762b034e5957ec6bdfb7a
SofiiaNikolenko <sofii.nikolenko@gmail.com>
2024-05-13 13:51:03 +0200

    Update trip.js

--- models/trip.js
+++ models/trip.js
@@ -3,30 +3,6 @@ const Joi = require("joi");
 
 const { handleMongooseError } = require("../helpers");
 
-// const tripSchema = new Schema(
-//   {
-//     name: {
-//       type: String,
-//       required: [true, "Set name for trip"],
-//     },
-//     email: {
-//       type: String,
-//     },
-//     phone: {
-//       type: String,
-//     },
-//     favorite: {
-//       type: Boolean,
-//       default: false,
-//     },
-//     owner: {
-//       type: Schema.Types.ObjectId,
-//       ref: "user",
-//     },
-//   },
-//   { versionKey: false, timestamps: true }
-// );
-
 const tripSchema = new Schema(
   {
     owner: {
@@ -83,6 +59,7 @@ const tripSchema = new Schema(
 tripSchema.post("save", handleMongooseError);
 
 const addSchema = Joi.object({
+  owner: Joi.string().required(),
   title: Joi.string().required().messages({
     "string.base": "title must be a string",
     "string.empty": "title cannot be empty",
@@ -109,31 +86,6 @@ const addSchema = Joi.object({
   // ),
 });
 
-// const addSchema = Joi.object({
-//   name: Joi.string().min(2).max(50).required().messages({
-//     "string.base": "name must be a string",
-//     "string.empty": "name cannot be empty",
-//     "string.min": "name must be at least 2 characters",
-//     "string.max": "name cannot be more than 50 characters",
-//     "any.required": "name is a required field",
-//   }),
-//   email: Joi.string().email().required().messages({
-//     "string.base": "email must be a string",
-//     "string.empty": "email cannot be empty",
-//     "string.email": "email must be a valid email",
-//     "any.required": "email is a required field",
-//   }),
-//   phone: Joi.string()
-//     .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
-//     .required()
-//     .messages({
-//       "string.base": "phone must be a string",
-//       "string.empty": "phone cannot be empty",
-//       "string.pattern.base": "phone must be in the format (111) 111-1111",
-//       "any.required": "phone is a required field",
-//     }),
-//   favorite: Joi.boolean(),
-// });
 
 const updateFavoriteSchema = Joi.object({
   favorite: Joi.boolean().required(),
